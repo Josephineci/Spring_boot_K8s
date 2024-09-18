6814a810acf47e7155baf17e9b8d870331bec993
Ubuntu <ubuntu@ip-172-31-94-223.ec2.internal>
2024-09-18 10:57:10 +0000

    ci/changes to scripts

--- AWS_setup/install_docker_minikube.sh
+++ AWS_setup/install_docker_minikube.sh
@@ -1,22 +1,35 @@
 #!/bin/bash
 
-# Switch to root user
-sudo su
-
 # Update the system
-yum update -y
+apt-get update -y
 
 # Install Docker
-yum install docker -y
+# Add Docker's official GPG key:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Install Docker packages
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
 
 # Enable Docker to start on boot
-systemctl enable docker
+sudo systemctl enable docker
 
 # Start Docker service
-systemctl start docker
+sudo systemctl start docker
 
 # Check Docker service status
-systemctl status docker
+sudo systemctl status docker
 
 # Check Docker version
 docker --version
--- AWS_setup/install_kubectl.sh
+++ AWS_setup/install_kubectl.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# Install curl command
+sudo apt install curl -y
+
 # Download the latest stable release of kubectl
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+
 
 # Install kubectl and set the appropriate permissions
 sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
